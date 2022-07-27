875a559b06c71f9915285e26d7824634a5268b0c
Boldizsar <boldizsar.kormendy@gmail.com>
2022-07-27 09:53:22 +0200

    Refactored some stuff

--- models/user.py
+++ models/user.py
@@ -2,19 +2,18 @@ from db import db
 
 
 class UserModel(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80))
+    username = db.Column(db.String(80), unique=True)
     password = db.Column(db.String(80))
 
     def __init__(self, username, password):
         self.username = username
         self.password = password
 
-    def save_to_db(self):
-        db.session.add(self)
-        db.session.commit()
+    def json(self):
+        return {"id": self.id, "username": self.username}
 
     @classmethod
     def find_by_username(cls, username):
@@ -23,3 +22,11 @@ class UserModel(db.Model):
     @classmethod
     def find_by_id(cls, _id):
         return cls.query.filter_by(id=_id).first()
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
